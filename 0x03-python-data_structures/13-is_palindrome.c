013044f52a47bc29d5314f1437061f8587aa5457
Hailu Worku <hailuworku1@yahoo.com>
2022-06-05 16:31:25 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 /**
  * is_palindrome - checks if palindrome
@@ -22,4 +20,3 @@ int is_palindrome(listint_t **head)
 	}
 	return (0);
 }
-
