50b74d3c8b21c0e9d951c5142e9fa5e8250f5ba3
Hailu Worku <hailuworku1@yahoo.com>
2022-07-04 10:26:19 +0300

    Create 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module contains the function is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if the object is the same"""
+    return (type(obj) == a_class)
