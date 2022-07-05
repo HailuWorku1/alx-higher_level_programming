daa1cd6122a68d2419916a639740454e2a5826d0
Hailu Worku <hailuworku1@yahoo.com>
2022-07-05 20:53:00 +0300

    Update and rename 2-append_write.py to 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+"""append_to_file"""
+
 
 def append_write(filename="", text=""):
-    with open(filename, 'a+', encoding="utf-8") as f:
-        return f.write(text)
+    """appends a string to the end of a file"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)    
