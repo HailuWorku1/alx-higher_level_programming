af9e37aa93f476a16dc2a6e50418cc13f174c113
Hailu Worku <hailuworku1@yahoo.com>
2022-07-05 13:36:29 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
         filename (str): filename/filepath
     """
     if type(filename) is str:
-        with open(filename, 'r') as fyle:
+        with open(filename, 'r', encoding="UTF-8") as fyle:
             print(fyle.read(), end='')
