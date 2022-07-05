8215712d1bc6ad787bbd1d1833a3f8d14bf3aef6
Hailu Worku <hailuworku1@yahoo.com>
2022-07-05 20:02:47 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,5 +2,5 @@ def write_file(filename="", text=""):
     """
     Write a string to text file and returns the nb of characters
     """
-    with open(filename, mode="w", encoding="UTF-8") as file:
+    with open(filename, mode="w+", encoding="UTF-8") as file:
         return file.write(text)
