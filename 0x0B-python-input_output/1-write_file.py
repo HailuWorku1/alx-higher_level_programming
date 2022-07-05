3bad6f92932140cd9f5613e9d735fbe9e280ec43
Hailu Worku <hailuworku1@yahoo.com>
2022-07-05 13:46:08 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,6 @@
 def write_file(filename="", text=""):
-    """Writes text to filename
-    Args:
-        filename (str): filename/filepath
-        text (str): text to write
     """
-    count = 0 
-    if type(filename) is str and type(text) is str:
-        with open(filename, 'w') as fyle:
-            count = fyle.write(text)
-    return count
+    Write a string to text file and returns the nb of characters
+    """
+    with open(filename, mode="w", encoding="UTF-8") as file:
+        return file.write(text)
